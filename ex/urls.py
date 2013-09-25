e114bc17285daaff639ccff63b214c2be0b3ed17
SamR <sam@sam-virtual-machine.(none)>
2013-09-25 04:32:23 -0700

    my initial files

--- ex/urls.py
+++ ex/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
-
 from ex import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^index.html$', views.index, name='index'),
+    url(r'^test.html*$', views.testPage),
 )
--- ex/views.py
+++ ex/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.template.loader import get_template
+
 
 def index(request):
     return HttpResponse('Hello, world. Russ was here');
+
+def testPage(request):
+    t = get_template('test.html')
+    headerText='our header will go in here'
+    html = t.render(Context({'header': headerText}))
+    return HttpResponse(html)
+
