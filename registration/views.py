4f2fb826ccb86ae4ab03bff3b4ed47e574030c8a
Joel Brady <jbrady@cse.unsw.edu.au>
2013-09-26 01:02:41 +1000

    basic registration form

--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django import forms
 
 
-def index(request):
-    return HttpResponse('This will be the registration page')
+class RegistrationForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+
+def register(request):
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            return render(request, 'register_result.html', form.cleaned_data)
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
