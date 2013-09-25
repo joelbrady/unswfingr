2b736ff686e0202cf132a2430883f6e97a0bc325
sam-roberts <s4mps0n@gmail.com>
2013-09-25 06:17:44 -0700

    fixed up test page

--- ex/views.py
+++ ex/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import Context
 from django.template.loader import get_template
 
@@ -7,8 +8,5 @@ def index(request):
     return HttpResponse('Hello, world. Russ was here');
 
 def testPage(request):
-    t = get_template('test.html')
-    headerText='our header will go in here'
-    html = t.render(Context({'header': headerText}))
-    return HttpResponse(html)
+    return render(request, 'test.html');
 
