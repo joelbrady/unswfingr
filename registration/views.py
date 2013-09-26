39e85365ead05db22869cebf6973051e7ec19098
Joel Brady <jbrady@cse.unsw.edu.au>
2013-09-27 01:19:44 +1000

    fingr-4: user can sign up with email and password, saves them as a user in the database

--- registration/views.py
+++ registration/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
-from django import forms
-
-
-class RegistrationForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput())
+from django.contrib.auth.models import User
+from registration.forms import RegistrationForm
 
 
 def register(request):
     if request.POST:
+        # load up the form with data from the POST request
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            return render(request, 'register_result.html', form.cleaned_data)
+            user = User.objects.create_user(username=form.cleaned_data['email'], email=form.cleaned_data['email'],
+                                            password=form.cleaned_data['password'])
+            return render(request, 'register_result.html', {'email': user.username})
+        else:
+            print form.errors
     else:
         form = RegistrationForm()
+
     return render(request, 'register.html', {'form': form})
