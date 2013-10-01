d96197b13feecfc975619d3bacd37f3353963aa5
sam-roberts <s4mps0n@gmail.com>
2013-10-01 05:30:30 -0700

    proper home page

--- ex/views.py
+++ ex/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import Context
+from django.template import Context, RequestContext, loader
 from django.template.loader import get_template
 
 
@@ -8,5 +8,10 @@ def index(request):
     return HttpResponse('Hello, world. Russ was here');
 
 def testPage(request):
-    return render(request, 'test.html');
+
+    template = loader.get_template('test.html')
+    context = RequestContext(request, {
+        'logged_in': False,
+    })
+    return HttpResponse(template.render(context))
 
