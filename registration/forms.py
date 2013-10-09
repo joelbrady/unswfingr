aa756a7ea7d0ab7acfc39bdc5d720df75b2f5b5c
sam-roberts <s4mps0n@gmail.com>
2013-10-09 05:21:04 -0700

    got registration to work

--- registration/forms.py
+++ registration/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth.hashers import MAXIMUM_PASSWORD_LENGTH
 
 
 class RegistrationForm(forms.Form):
-    email = forms.EmailField(max_length=30)
-    password = forms.CharField(widget=forms.PasswordInput(), max_length=MAXIMUM_PASSWORD_LENGTH)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput(), max_length=MAXIMUM_PASSWORD_LENGTH)
+    email = forms.EmailField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}), max_length=MAXIMUM_PASSWORD_LENGTH)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput(attrs={'class':'form-control'}), max_length=MAXIMUM_PASSWORD_LENGTH)
 
     # django automatically calls clean_ for each vield in the form
     # either return the cleaned value, or raise a forms.ValidationError
