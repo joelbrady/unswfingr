c1bfd6672432c8e8de94df85105335fb71e9d297
sam-roberts <s4mps0n@gmail.com>
2013-10-10 15:12:24 +1100

    header works correctly, migrated to master base

--- main/forms.py
+++ main/forms.py
@@ -3,5 +3,5 @@ from django.contrib.auth.hashers import MAXIMUM_PASSWORD_LENGTH
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(max_length=30)
-    password = forms.CharField(widget=forms.PasswordInput(), max_length=MAXIMUM_PASSWORD_LENGTH)
+    email = forms.EmailField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}), max_length=MAXIMUM_PASSWORD_LENGTH)
