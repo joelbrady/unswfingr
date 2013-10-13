eeebc7f26760af0dcf2e835f2c0c8809021f3583
sai <sai96@hotmail.com>
2013-10-13 18:39:20 +1100

    in the process of adding dynamic forms

--- profile/urls.py
+++ profile/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from profile import views
 
 urlpatterns = patterns('',
+    url(r'^profile_course.html$', views.edit_course),
     url(r'^$', views.edit_profile),
 )
