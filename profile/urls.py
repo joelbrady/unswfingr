b26a7a7cd7f76e5c3f7168889f7da4961e428874
Joel Brady <jbrady@cse.unsw.edu.au>
2013-10-16 15:45:57 +1100

    fixed crash when opening home page when logged in

--- profile/urls.py
+++ profile/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from profile import views
 
 urlpatterns = patterns('',
-    url(r'^$',views.index),
+    url(r'^$', views.index),
     url(r'^profile_course.html$', views.edit_course),
-    #url(r'^$', views.edit_profile),
+    url(r'^edit_profile$', views.edit_profile),
 )
