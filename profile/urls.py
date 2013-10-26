76f907f06d1261b84f6e454668494113be78e08c
Joel Brady <jbrady@cse.unsw.edu.au>
2013-10-27 02:19:44 +1100

    Removed extra slash in profile url

--- profile/urls.py
+++ profile/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from profile import views
 
 urlpatterns = patterns('',
-    url(r'/(\d+)/?$',views.view_profile),
+    url(r'(\d+)/?$',views.view_profile),
     url(r'^edit_courses', views.edit_course),
     url(r'^edit_profile', views.edit_profile),
 )
