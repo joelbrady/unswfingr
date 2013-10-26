84c16416ab820c499c7bb0ce962365a81121e420
Joel Brady <jbrady@cse.unsw.edu.au>
2013-10-26 14:31:31 +1100

    Changed url for map page.

--- map/urls.py
+++ map/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from map import views
 
 urlpatterns = patterns('',
-    url(r'^view$', views.view_map),
+    url(r'^$', views.view_map),
     url(r'^new_static$', views.new_static_marker),
     url(r'^update_static$', views.update_static_marker),
     url(r'^get_static', views.get_static_markers),
