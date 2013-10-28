9e0a2f896a50dec7c6a402e6776be99b2798384d
Joel Brady <jbrady@cse.unsw.edu.au>
2013-10-28 13:44:17 +1100

    Can see friends locations on map. Can select them (panTo them)

--- map/views.py
+++ map/views.py
@@ -145,4 +145,11 @@ def get_friends_locations(request):
     user = user_to_fingr(request.user)
     assert user is not None
 
-    return HttpResponse(json.dumps(map(model_to_dict, [friend.my_location for friend in user.friends_list.all()])))
+    response = []
+
+    for friend in user.friends_list.all():
+        loc = model_to_dict(friend.my_location)
+        loc['name'] = friend.full_name
+        response.append(loc)
+
+    return HttpResponse(json.dumps(response))
--- registration/models.py
+++ registration/models.py
@@ -38,7 +38,7 @@ class FingrUser(models.Model):
         (visibility_friends, 'Friends'),
         (visibility_all, 'All'),
     )
-    visibility = models.CharField(max_length=7, choices=visibility_choices,default=visibility_all)
+    visibility = models.CharField(max_length=7, choices=visibility_choices, default=visibility_all)
 
     def _get_username(self):
         return self.django_user.username
@@ -53,7 +53,7 @@ class FingrUser(models.Model):
         return self.messages.all()
 
     def _full_name(self):
-        if (self.first_name):
+        if self.first_name:
             return self.first_name + " " + self.last_name
         else:
             return self.username
